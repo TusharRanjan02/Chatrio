e34443a5a8e678821e0247798250abda8074fca4
TusharRanjan02 <tusharranjandas26@gmail.com>
2025-08-04 22:19:17 +0530

    fronted routes initialise

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser, { signedCookie } from "cookie-parser"
+
+import cors from "cors"
+
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
@@ -11,6 +14,10 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true  // allow frontend to send the cookies
+}))
 app.use(express.json());
 app.use(cookieParser())
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route } from "react-router"
+import HomePage from "./pages/HomePage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import NotificationsPage from "./pages/NotificationsPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import {Toaster} from "react-hot-toast"
+import { useQuery } from '@tanstack/react-query'
+import axios from "axios"
+import {axiosInstance} from "./lib/axios.js"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const {data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+
+    queryFn: async() => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data
+    }
+  })
+
+  console.log(data)
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='h-screen' data-theme="night">
+      <Routes>
+          <Route path='/' element = {<HomePage />} />
+          <Route path='/login' element = {<LoginPage />} />
+          <Route path='/signup' element = {<SignUpPage />} />
+          <Route path='/onboarding' element = {<OnboardingPage />} />
+          <Route path='/notifications' element = {<NotificationsPage />} />
+          <Route path='/chat' element = {<ChatPage />} />
+          <Route path='/call' element = {<CallPage />} />
+      </Routes>
+
+      <Toaster />
+    </div>
     </>
   )
 }
