777d14f4949d225d94b6345b0c0f489fccdbf8dc
TusharRanjan02 <tusharranjandas26@gmail.com>
2025-08-04 13:39:35 +0530

    backend part done

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser, { signedCookie } from "cookie-parser"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/chat.route.js"
+import { connectDB } from "./lib/db.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${5001}`)
+    connectDB();
+})
